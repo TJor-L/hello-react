b654879814e325a641edadf88c3fd51f5926a2f8
TJor-L <l.tingjun@wustl.edu>
2025-04-01 18:43:06 -0500

    delete original APP function and return Square

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,15 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-    </>
-  )
+export default function Square() {
+    return <button className="square">X</button>;
 }
 
-export default App
