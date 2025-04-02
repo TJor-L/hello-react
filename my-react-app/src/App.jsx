7086694dd175a19ea81092c205f2d649c13fa25a
TJor-L <l.tingjun@wustl.edu>
2025-04-01 21:40:17 -0500

    added unique key to move to help react rendering

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -89,17 +89,17 @@ export function Board({ xIsNext, squares, onPlay }) {
     }
 
     const moves = history.map((squares, move) => {
-    let description;
-    if (move > 0) {
-        description = 'Go to move #' + move;
-    } else {
-        description = 'Go to game start';
-    }
-    return (
-        <li>
-        <button onClick={() => jumpTo(move)}>{description}</button>
-        </li>
-    );
+        let description;
+        if (move > 0) {
+            description = 'Go to move #' + move;
+        } else {
+            description = 'Go to game start';
+        }
+        return (
+            <li key={move}>
+                <button onClick={() => jumpTo(move)}>{description}</button>
+            </li>
+        );
     });
 
     return (
